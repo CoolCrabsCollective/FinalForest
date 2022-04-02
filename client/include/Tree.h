4e0e7ff892c9fe0fe5bdd980a824106005b61bd5
Cedric Martens <cedricmartens98@gmail.com>
2022-04-02 10:09:19 -0400

    Add tree class

---
+++ client/include/Tree.h
@@ -0,0 +1,17 @@
+//
+// Created by cedric on 2022-04-02.
+//
+
+#ifndef LD50_TREE_H
+#define LD50_TREE_H
+
+
+class Tree {
+private:
+    int health;
+public:
+    Tree();
+};
+
+
+#endif //LD50_TREE_H
---
+++ client/src/Tree.cpp
@@ -0,0 +1,9 @@
+//
+// Created by cedric on 2022-04-02.
+//
+
+#include "../include/Tree.h"
+
+Tree::Tree() {
+
+}
