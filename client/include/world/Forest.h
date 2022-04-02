aec2316277eb625c2435750784c80a0ff8a1af0b
Cedric Martens <cedricmartens98@gmail.com>
2022-04-02 13:44:34 -0400

    failed to add grass

--- client/include/world/Forest.h
+++ client/include/world/Forest.h
@@ -6,17 +6,24 @@
 #define LD50_CLIENT_FOREST_H
 
 
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
 #include "Tickable.h"
 #include "SFML/Graphics/Drawable.hpp"
 #include "WIZ/asset/AssetLoader.h"
 #include "Dynamics/b2World.h"
 #include "Entity.h"
 
+#define TILES_WIDTH 32
+#define TILES_HEIGHT 32
+
 class Forest : public sf::Drawable, public Tickable {
 	const wiz::AssetLoader& assetLoader;
 	b2World world;
 
 	std::vector<Entity*> objects;
+    sf::Sprite grass_sprite[4];
+    int grass_map[TILES_HEIGHT][TILES_WIDTH];
 public:
     int nutCount;
 
@@ -25,6 +32,8 @@ public:
 	~Forest() override;
 
 	void draw(sf::RenderTarget& target, const sf::RenderStates& states) const override;
+
+	void createForest();
 
 	b2World& getB2World();
 
--- client/src/world/Forest.cpp
+++ client/src/world/Forest.cpp
@@ -9,11 +9,72 @@
 #include "world/Squirrel.h"
 #include <stdlib.h>
 #include <iostream>
+#include "GameAssets.h"
 
 Forest::Forest(const wiz::AssetLoader& assetLoader) : assetLoader(assetLoader), world(b2Vec2_zero) {
+
+    nutCount = 0;
+    grass_sprite[0] = sf::Sprite(*assetLoader.get(GameAssets::GRASS1));
+    grass_sprite[1] = sf::Sprite(*assetLoader.get(GameAssets::GRASS2));
+    grass_sprite[2] = sf::Sprite(*assetLoader.get(GameAssets::GRASS3));
+    grass_sprite[3] = sf::Sprite(*assetLoader.get(GameAssets::GRASS4));
+
+    float scale = grass_sprite->getScale().x / grass_sprite->getTexture()->getSize().x;
+    for(int i = 0; i < 4; i++)
+    {
+        grass_sprite->setScale({scale, scale});
+    }
+    for(int i = 0; i < TILES_HEIGHT; i++)
+    {
+        for(int j = 0; j < TILES_WIDTH; j++)
+        {
+            grass_map[i][j] = rand() % 4;
+        }
+    }
+    createForest();
+}
+
+Forest::~Forest() {
+	for(Entity* entity : objects)
+		delete entity;
+	objects.clear();
+}
+
+void Forest::tick(float delta) {
+	for(Entity* obj : objects) {
+		Tickable* tickable = dynamic_cast<Tickable*>(obj);
+		if(tickable)
+			tickable->tick(delta);
+	}
+
+	world.Step(delta / 1000.0f, 6, 2);
+}
+
+void Forest::draw(sf::RenderTarget& target, const sf::RenderStates& states) const {
+
+	for(int i = 0; i < TILES_HEIGHT; i++)
+    {
+	    for(int j = 0; j < TILES_WIDTH; j++)
+        {
+            sf::Sprite grass = grass_sprite[grass_map[i][j]];
+            grass.setPosition({j * 16.f, i * 16.f});
+	        //target.draw(grass, states);
+        }
+    }
+
+    for(Entity* obj : objects) {
+		sf::Drawable* drawable = dynamic_cast<sf::Drawable*>(obj);
+		if(drawable)
+			target.draw(*drawable, states);
+	}
+}
+
+void Forest::createForest() {
     float minDistance = 8.f;
     int totalTrees = 75;
     std::vector<Tree *> trees;
+
+
     while (trees.size() < totalTrees) {
         float x = (float) (rand() % 100);
         float y = (float) (rand() % 100);
@@ -37,34 +98,9 @@ Forest::Forest(const wiz::AssetLoader& assetLoader) : assetLoader(assetLoader), 
     for(Tree* tree : trees)
         objects.push_back(tree);
 
-    nutCount = 0;
 
-	objects.push_back(new Squirrel(*this, b2Vec2(0.0f, 0.0f)));
-}
 
-Forest::~Forest() {
-	for(Entity* entity : objects)
-		delete entity;
-	objects.clear();
-}
-
-void Forest::tick(float delta) {
-	for(Entity* obj : objects) {
-		Tickable* tickable = dynamic_cast<Tickable*>(obj);
-		if(tickable)
-			tickable->tick(delta);
-	}
-
-	world.Step(delta / 1000.0f, 6, 2);
-}
-
-void Forest::draw(sf::RenderTarget& target, const sf::RenderStates& states) const {
-
-	for(Entity* obj : objects) {
-		sf::Drawable* drawable = dynamic_cast<sf::Drawable*>(obj);
-		if(drawable)
-			target.draw(*drawable, states);
-	}
+    objects.push_back(new Squirrel(*this, b2Vec2(0.0f, 0.0f)));
 }
 
 b2World& Forest::getB2World() {
