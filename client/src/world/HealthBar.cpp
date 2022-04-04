6d7f0a1e5cdbddc4fabd1ea22d9c96b6aa0076cf
Adrien Tremblay <adrien@adrientremblay.com>
2022-04-03 21:26:07 -0400

    tweak health bar colors and height

--- client/src/world/HealthBar.cpp
+++ client/src/world/HealthBar.cpp
@@ -13,13 +13,13 @@ HealthBar::HealthBar(Physical* physicalTarget, Damageable* damageable, const wiz
 
 void HealthBar::draw(sf::RenderTarget &target, const sf::RenderStates &states) const {
     barSprite.setPosition({physicalTarget->getPosition().x, 100 - physicalTarget->getPosition().y});
-    // draw white background
-    barSprite.setColor(sf::Color(sf::Color(255, 255, 255, 255)));
-    barSprite.setScale({10, 1});
+    // draw background
+    barSprite.setColor(sf::Color(sf::Color::Red));
+    barSprite.setScale({10, 0.5f});
     target.draw(barSprite);
 
     // draw red fill
-    barSprite.setColor(sf::Color(sf::Color(255, 0, 0, 255)));
+    barSprite.setColor(sf::Color(sf::Color::Green));
     barSprite.setScale({damageable->getHealth(), 1});
     target.draw(barSprite);
 }
