bfd54e74a80fc7118c1bbf929e5847169649379a
Adrien Tremblay <adrien@adrientremblay.com>
2022-04-04 15:26:30 -0400

    tree health bar color

--- client/src/world/HealthBar.cpp
+++ client/src/world/HealthBar.cpp
@@ -9,6 +9,15 @@
 
 HealthBar::HealthBar(Physical* physicalTarget, Damageable* damageable, const wiz::AssetLoader& assetLoader) : physicalTarget(physicalTarget), damageable(damageable){
     barSprite = sf::Sprite((*assetLoader.get(GameAssets::WHITE_PIXEL)));
+
+    treeHealthBar = dynamic_cast<Tree*>(damageable) != NULL;
+    if (treeHealthBar) {
+        backgroundColor = sf::Color(0xBBBBBBFF);
+        foregroundColor = sf::Color::White;
+    } else {
+        backgroundColor = sf::Color::Red;
+        foregroundColor = sf::Color::Green;
+    }
 }
 
 void HealthBar::draw(sf::RenderTarget &target, const sf::RenderStates &states) const {
@@ -20,12 +29,12 @@ void HealthBar::draw(sf::RenderTarget &target, const sf::RenderStates &states) c
 
     barSprite.setPosition({physicalTarget->getPosition().x - (barWidth / 2), 100 - physicalTarget->getPosition().y - 5});
     // draw background
-    barSprite.setColor(sf::Color(sf::Color::Red));
+    barSprite.setColor(backgroundColor);
     barSprite.setScale({barWidth, barHeight});
     target.draw(barSprite);
 
     // draw red fill
-    barSprite.setColor(sf::Color(sf::Color::Green));
+    barSprite.setColor(sf::Color(foregroundColor));
     barSprite.setScale({(damageable->getHealth() / damageable->maxHealth) * barWidth, barHeight});
     target.draw(barSprite);
 }
