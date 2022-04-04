2853494fec895a88ebd4f63a582f2e5d2773cbc7
Adrien Tremblay <adrien@adrientremblay.com>
2022-04-03 21:15:31 -0400

    fix position of health bars for trees

--- client/src/world/HealthBar.cpp
+++ client/src/world/HealthBar.cpp
@@ -12,8 +12,7 @@ HealthBar::HealthBar(Physical* physicalTarget, Damageable* damageable, const wiz
 }
 
 void HealthBar::draw(sf::RenderTarget &target, const sf::RenderStates &states) const {
-
-    barSprite.setPosition({physicalTarget->getPosition().x, physicalTarget->getPosition().y});
+    barSprite.setPosition({physicalTarget->getPosition().x, 100 - physicalTarget->getPosition().y});
     // draw white background
     barSprite.setColor(sf::Color(sf::Color(255, 255, 255, 255)));
     barSprite.setScale({10, 1});
